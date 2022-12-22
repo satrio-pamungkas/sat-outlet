b1825f63e71f396abc1f3f3ba5ab6e99188b44b2
Muhammad Raihan Satrio <adewipo@gmail.com>
2022-12-22 10:40:54 +0700

    feat: added Product.API Controller

--- Services/Product.API/Models/Product.cs
+++ Services/Product.API/Models/Product.cs
@@ -5,6 +5,7 @@ public class Product
     public Guid Id { get; set; }
     public String? Name { get; set; }
     public int? Price { get; set; }
+    public int? Quantity { get; set; }
     public DateTime CreatedDate { get; set; }
     public DateTime UpdatedDate { get; set; }
 }
